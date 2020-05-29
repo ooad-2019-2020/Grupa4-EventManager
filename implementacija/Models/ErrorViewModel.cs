675e6582442844996cb86014ad913377826e9e16
MedinaKapo <mkapo2@etf.unsa.ba>
2020-05-30 01:54:38 +0200

    dodana implementacija

---
+++ implementacija/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace implementacija.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
