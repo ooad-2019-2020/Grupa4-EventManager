0509439db72f63c99593df6310b17e8780ef8f47
MedinaKapo <mkapo2@etf.unsa.ba>
2020-05-30 02:11:34 +0200

    atributi,getteri i settreri korisnika

--- implementacija/Models/Korisnik.cs
+++ implementacija/Models/Korisnik.cs
@@ -7,5 +7,14 @@ namespace implementacija.Models
 {
     public class Korisnik
     {
+public static int ID { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public Boolean trenutnoAktivan { get; set; }
+        public String adresa { get; set; }
+        public ICollection<Obavijest> listaObavjestenja { get; set; }
+
+
     }
 }
