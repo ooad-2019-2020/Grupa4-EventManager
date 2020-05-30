5fa7cae43a4c800900d8bdc2c0ff4a4347e40591
MedinaKapo <mkapo2@etf.unsa.ba>
2020-05-30 02:19:41 +0200

    atributi,getteri,setteri fizickog lica

--- implementacija/Models/FizickoLice.cs
+++ implementacija/Models/FizickoLice.cs
@@ -7,5 +7,17 @@ namespace implementacija.Models
 {
     public class FizickoLice
     {
+        public string ime { get; set; }
+        public string prezime { get; set; }
+        public string brojKartice { get; set; }
+        public Boolean osobaOdgovornaUImeUstanove { get; set; }
+        public DateTime datumRodjenja { get; set; }
+        internal IFizckoLice TipFizickogLica { get; set; }
+        public ICollection<Transakcija> listaTransakcija { get; set; }
+        public ICollection<Dogadjaj> wishListaDogadjaja { get; set; }
+        public ICollection<Rezervacija> listaZavrsenihRezervacija { get; set; }
+        public double stanjeRacuna { get; set; }
+        public int popust { get; set; }
+        public ICollection<Rezervacija> listaRezervacija { get; set; }
     }
 }
