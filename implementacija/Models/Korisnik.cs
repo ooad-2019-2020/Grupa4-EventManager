21986f95c753c1f31a12d5bec9dcbf314ae8b730
MedinaKapo <mkapo2@etf.unsa.ba>
2020-05-30 03:14:18 +0200

    dodan proxy

--- implementacija/Models/Korisnik.cs
+++ implementacija/Models/Korisnik.cs
@@ -7,15 +7,15 @@ namespace implementacija.Models
 {
     public class Korisnik
     {
-public static int ID { get; }
+public static int ID { get; set; }
         public string username { get; set; }
         public string password { get; set; }
         public string email { get; set; }
-        public string ime { get; set; }
-        public DateTime datumPrijave { get; set; }
+        public Boolean trenutnoAktivan { get; set; }
         public String adresa { get; set; }
-       
+        public IList<Obavijest> listaObavjestenja { get; set; }
 
-
+        public virtual ICollection<Komentar> Komentar { get; set; }
+        public virtual ICollection<Proxy> Proxy { get; set; }
     }
 }
