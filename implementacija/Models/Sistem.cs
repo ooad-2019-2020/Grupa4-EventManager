a72940f6dd09e1e1a100f975d230b3b3239371f2
MedinaKapo <mkapo2@etf.unsa.ba>
2020-06-05 23:37:44 +0200

    sistem

---
+++ implementacija/Models/Sistem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace implementacija.Models
+{
+    public class Sistem
+    {
+        public int SistemId { get; set; }// ovo je zapravo brojac posjeta
+        public int KorisnikId { get; set; }
+
+        //dio u kojem se definisu veze sa ostalim klasama
+        public virtual ICollection<Korisnik> Korisnik { get; set; }
+
+
+    }
+}
