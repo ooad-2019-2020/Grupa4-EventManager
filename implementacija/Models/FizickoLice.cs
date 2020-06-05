d73ea3e6e447a80df8d7660f5e2c7da4657c9808
MedinaKapo <mkapo2@etf.unsa.ba>
2020-06-05 23:22:47 +0200

    dodan model za FL

---
+++ implementacija/Models/FizickoLice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace implementacija.Models
+{
+    public class FizickoLice
+    {
+        public int KorisnikId { get; set; }
+        public string ime { get; set; }
+        public string prezime { get; set; }
+        public string brojKartice { get; set; }
+        public DateTime datumRodjenja { get; set; }
+        public int tipFizickogLica { get; set; }
+        public Boolean VIP { get; set; }
+        public double stanjeRacuna { get; set; }
+        public Boolean odgovornoLice { get; set; }
+
+        //dio u kojem se definisu veze sa ostalim klasama
+        public virtual Korisnik Korisnik { get; set; }
+        public virtual TipFizickogLica TipFizickogLica { get; set; }
+
+    }
+}
