76475dfc3b3cd25c0a79afce4c2e9481014a0757
MedinaKapo <mkapo2@etf.unsa.ba>
2020-06-05 23:34:33 +0200

    dodano admin

---
+++ implementacija/Models/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace implementacija.Models
+{
+    public class Admin
+    {
+        public int KorisnikId { get; set; }
+
+        //dio u kojem se definisu veze sa ostalim klasama
+        public virtual Korisnik Korisnik { get; set; }
+        public virtual 
+    }
+}
