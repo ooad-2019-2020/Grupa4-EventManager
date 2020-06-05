ba9e6d9d423a7b77be6fbf10152c542a1991de7d
MedinaKapo <mkapo2@etf.unsa.ba>
2020-06-05 23:32:51 +0200

    arhiva ustanova

---
+++ implementacija/Models/ArhivaKorisnikaUstanova.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace implementacija.Models
+{
+    public class ArhivaKorisnikaUstanova
+    {
+        public int ArhivaKorisnikaUstanovaId { get; set; }
+        public int KorisnikId { get; set; }
+        public double imaoUplata { get; set; }
+        public DateTime kreiraoRacun { get; set; }
+        public DateTime napustioSistem { get; set; }
+
+        //dio u kojem se definisu veze sa ostalim klasama
+        public virtual Korisnik Korisnik { get; set; }
+
+    }
+}
