62e443e9ccc3ee922e843771b66a3d5558dcb242
MedinaKapo <mkapo2@etf.unsa.ba>
2020-06-06 00:21:27 +0200

    dodano u startup

--- implementacija/Startup.cs
+++ implementacija/Startup.cs
@@ -12,6 +12,7 @@ using implementacija.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using implementacija.Models;
 
 namespace implementacija
 {
@@ -27,9 +28,10 @@ namespace implementacija
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+           
+                services.AddDbContext<EMContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("DbConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
