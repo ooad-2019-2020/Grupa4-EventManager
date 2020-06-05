68b4df2c170e30cbb0ae6ad15d38253a0a2b712d
MedinaKapo <mkapo2@etf.unsa.ba>
2020-06-05 23:03:56 +0200

    dodan model za Korisnika

---
+++ implementacija/Models/Korisnik.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace implementacija.Models
+{
+    public class Korisnik
+    {
+        public int KorisnikId { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string ime { get; set; }
+        public DateTime datumPrijave { get; set; }
+        public string adresa { get; set; }
+        public string email { get; set; }
+
+        //dio u kojem se definisu veze sa ostalim  klasama
+        public virtual ICollection<ArhivaKorisnikaUstanova> ArhivaKorisnikaUstanova { get; set; }
+        public virtual ICollection<Zahtjev> Zahtjev { get; set; }
+        public virtual ICollection<ArhivaKorisnikaFL> ArhivaKorisnikaFL { get; set; }
+        public virtual ICollection<Admin> Admin { get; set; }
+        public virtual ICollection<Transakcija> Transakcija { get; set; }
+        public virtual ICollection<ArhivaDogadjaja> ArhivaDogadjaja { get; set; }
+        public virtual ICollection<Rezervacija> Rezervacija { get; set; }
+        public virtual ICollection<Obavijest> Obavijest { get; set; }
+        public virtual ICollection<Recenzija> Recenzija { get; set; }
+        public virtual ICollection<FizickoLice> FizickoLice { get; set; }
+        public virtual Sistem Sistem { get; set; }
+
+
+    }
+}
