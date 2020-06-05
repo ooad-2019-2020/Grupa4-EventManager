a6cad5b3ad3f25ab8e8677aab737eb2b48d3e2c5
MedinaKapo <mkapo2@etf.unsa.ba>
2020-06-05 23:27:22 +0200

    dodano za arhivu

---
+++ implementacija/Models/ArhivaKorisnikaFL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace implementacija.Models
+{
+    public class ArhivaKorisnikaFL
+    {
+        public int ArhivaKorisnikaFLId { get; set; }
+        public int KorisnikId { get; set; }
+        public int izvrsioTransakciju { get; set; }
+        public double ukupnoUplatio { get; set; }
+        public Boolean bioOdgovornoLice { get; set; }
+        public int TipFizickogLicaId { get; set; }
+        public DateTime kreiraoRacun { get; set; }
+        public DateTime napustioApp { get; set; }
+
+        //dio u kojem se definisu veze sa ostalim klasama
+        public virtual Korisnik Korisnik { get; set; }
+        public virtual TipFizickogLica TipFizickogLica { get; set; }
+
+    }
+}
