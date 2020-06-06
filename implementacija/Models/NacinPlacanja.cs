0078017f3c6b83933b004382423ad33ab06081d7
MedinaKapo <mkapo2@etf.unsa.ba>
2020-06-06 03:01:55 +0200

    nacin placanja

---
+++ implementacija/Models/NacinPlacanja.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace implementacija.Models
+{
+    public class NacinPlacanja
+    {
+        public int NacinPlacanjaId { get; set; }
+    }
+}
