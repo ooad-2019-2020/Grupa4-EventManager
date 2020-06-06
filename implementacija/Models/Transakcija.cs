e32b5109280192be6825b35a68a729ef573b5788
MedinaKapo <mkapo2@etf.unsa.ba>
2020-06-06 02:59:37 +0200

    transakcija

--- implementacija/Models/Transakcija.cs
+++ implementacija/Models/Transakcija.cs
@@ -7,5 +7,13 @@ namespace implementacija.Models
 {
     public class Transakcija
     {
+        public int TransakcijaId { get; set; }
+        public int KorisnikId { get; set; }
+        public double ukupnoZaUplatu { get; set; }
+        public int NacinPlacanjaId { get; set; }
+
+        //dio u kojem se definisu veze sa ostalim klasama
+        public virtual Korisnik Korisnik { get; set; }
+        public virtual NacinPlacanja NacinPlacanja { get; set; }
     }
 }
