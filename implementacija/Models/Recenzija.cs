9dca449b26bb54d153202134f0bc4fdd260e64e4
MedinaKapo <mkapo2@etf.unsa.ba>
2020-06-06 02:55:58 +0200

    komentar

--- implementacija/Models/Recenzija.cs
+++ implementacija/Models/Recenzija.cs
@@ -5,7 +5,19 @@ using System.Threading.Tasks;
 
 namespace implementacija.Models
 {
-    public class Recenzija
+    public class Komentar
     {
+        public int KomentarId { get; set; }
+        public string recenzija { get; set; }
+        public int brojZvjezdica { get; set; }
+        public int KorisnikId { get; set; }
+        public DateTime datumOstavljanja { get; set; }
+        public int DogadjajId { get; set; }
+        public bool neprimjerenKomentar { get; set; }
+
+        //dio u kojem se definisu veze sa ostalim klasama
+        public virtual Korisnik Korisnik { get; set; }
+        public virtual Dogadjaj Dogadjaj { get; set; }
+
     }
 }
