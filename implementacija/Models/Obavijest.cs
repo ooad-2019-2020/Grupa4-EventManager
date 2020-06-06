076504fab569b3348b2e98d0eaa06a2ad894c7b5
MedinaKapo <mkapo2@etf.unsa.ba>
2020-06-06 02:53:01 +0200

    obavijest

--- implementacija/Models/Obavijest.cs
+++ implementacija/Models/Obavijest.cs
@@ -7,5 +7,15 @@ namespace implementacija.Models
 {
     public class Obavijest
     {
+        public int ObavijestId { get; set; }
+        public int KorisnikId { get; set; }
+        public string tekstObavijesti { get; set; }
+        public DateTime datumSlanja { get; set; }
+        public int VrstaObavijestiId { get; set; }
+
+        //dio u kojem se definisu veze sa ostalim klasama
+        public virtual VrstaObavijesti VrstaObavijesti { get; set; }
+        public virtual Korisnik Korisnik { get; set; }
+        
     }
 }
