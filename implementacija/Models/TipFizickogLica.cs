240f93abcfc87be33245302d14bf5798dcfa485d
MedinaKapo <mkapo2@etf.unsa.ba>
2020-06-06 02:57:49 +0200

    TipFL

--- implementacija/Models/TipFizickogLica.cs
+++ implementacija/Models/TipFizickogLica.cs
@@ -7,5 +7,11 @@ namespace implementacija.Models
 {
     public class TipFizickogLica
     {
+        public int TipFizickogLicaId { get; set; }
+        public bool popust { get; set; }
+
+        //dio u kojem se definisu veze sa ostalim klasama
+        public virtual ICollection<FizickoLice> FizickoLice { get; set; }
+        public virtual ICollection<ArhivaKorisnikaFL> ArhivaKorisnikaFL { get; set; }
     }
 }
