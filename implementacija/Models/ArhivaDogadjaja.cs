98bef664ad17dc49529e55c2d475500c56c3b963
MedinaKapo <mkapo2@etf.unsa.ba>
2020-06-06 03:10:09 +0200

    arhiva dogadjaja

--- implementacija/Models/ArhivaDogadjaja.cs
+++ implementacija/Models/ArhivaDogadjaja.cs
@@ -7,6 +7,18 @@ namespace implementacija.Models
 {
     public class ArhivaDogadjaja
     {
+        public int ArhivaDogadjajaId { get; set; }
+        public int KorisnikId { get; set; }
+        public int UstanovaId { get; set; }
+        public double cijenaRezervacije { get; set; }
+        public bool popust { get; set; }
+        public int iznosPopusta { get; set; }
+        public int DogadjajId { get; set; }
+
+        //dio u kojem se definisu veze sa ostalim klasama
+        public virtual Korisnik Korisnik { get; set; }
+        public virtual Ustanova Ustanova { get; set; }
+        public virtual ICollection<Dogadjaj> Dogadjaj { get; set; }
 
     }
 }
