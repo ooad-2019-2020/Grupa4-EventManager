894e12def2e1f8acd434fee0ffa13a99cc5239a6
MedinaKapo <mkapo2@etf.unsa.ba>
2020-06-07 15:13:55 +0200

    popravljen vip

--- EventManager-Implementacija/implementacija/Models/VIP.cs
+++ EventManager-Implementacija/implementacija/Models/VIP.cs
@@ -9,17 +9,30 @@ namespace implementacija.Models
     {
         public int VIPId { get; set; }
         public int KorisnikId { get; set; }
+        [DisplayName("Ime")]
         public string ime { get; set; }
+        [DisplayName("Prezime")]
         public string prezime { get; set; }
+        [DisplayName("Broj kartice")]
         public string brojKartice { get; set; }
+        [DisplayName("Datum rođenja")]
         public DateTime datumRodjenja { get; set; }
+        [DisplayName("Tip fizičkog lica")]
         public int tipFizickogLica { get; set; }
         public double stanjeRacuna { get; set; }
         public Boolean odgovornoLice { get; set; }
         public Boolean uplatioClanarinu { get; set; }
         public double iznosClanarine { get; set; }
         public int trajanjeClanarine { get; set; }
-        
+        [DisplayName("Username")]
+        public string username { get; set; }
+        [DisplayName("Password")]
+        public string password { get; set; }
+        [DisplayName("Adresa")]
+        public string adresa { get; set; }
+        [DisplayName("Email")]
+        public string email { get; set; }
+
 
 
         //dio u kojem se definisu veze sa ostalim klasama
